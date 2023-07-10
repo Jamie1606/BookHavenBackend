b9637dc3c2b200f9d623fb43204469c56193d8a3
Zay Yar Tun <zyt.tharthar@gmail.com>
2023-07-11 01:08:33 +0800

    Image10072023

--- src/main/java/com/bookshop/bookhaven/model/DatabaseConnection.java
+++ src/main/java/com/bookshop/bookhaven/model/DatabaseConnection.java
@@ -2,7 +2,7 @@
 // Admin No		: 2235035, 2235022
 // Class		: DIT/FT/2A/02
 // Group		: 10
-// Date			: 6.7.2023
+// Date			: 10.7.2023
 // Description	: database configuration
 
 package com.bookshop.bookhaven.model;
@@ -13,15 +13,16 @@ import java.sql.DriverManager;
 public class DatabaseConnection {
 	public static Connection getConnection() {
 		// database configuration
-		String connURL = "jdbc:postgresql://satao.db.elephantsql.com/mhekoapk";
-		String db_username = "mhekoapk";
-		String db_password = "o9w2O25Afleif9CCVCEBDQZX4tT79MH7";
+		String connURL = "jdbc:postgresql://bookhaven.clcf0fzaiwt0.us-east-2.rds.amazonaws.com:5432/postgres";
+		String db_username = "postgres";
+		String db_password = "bookhaven";
 		
 		Connection conn = null;
 		
 		try {
 			Class.forName("org.postgresql.Driver");
 			conn = DriverManager.getConnection(connURL, db_username, db_password);
+			conn.setSchema("bookhavendb");
 		}
 		catch(Exception e) {
 			e.printStackTrace();
