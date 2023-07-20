1d2303f316bad4b1e42ce7f9c1df9ba8b2a4065d
thu-htetsan <THUHTET12.LMF@gmail.com>
2023-07-20 21:38:05 +0800

    20/7/2023Commit

--- src/main/java/com/bookshop/bookhaven/model/Genre.java
+++ src/main/java/com/bookshop/bookhaven/model/Genre.java
@@ -1,5 +1,30 @@
+//Author 	  : Thu Htet San
+//Admin No    : 2235022
+//Class       : DIT/FT/2A/02
+//Group       : 10
+//Date		  : 7.6.2023
+//Description : to store Genre data from database
+
 package com.bookshop.bookhaven.model;
 
 public class Genre {
 
+	private int genreID;
+	private String genre;
+
+	public int getGenreID() {
+		return genreID;
+	}
+
+	public void setGenreID(int genreID) {
+		this.genreID = genreID;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
 }
